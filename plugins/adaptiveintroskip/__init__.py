fb4cb69ce1d2c4e83a16b90a5714df3c2c5973c3
honue <zouhy2001@gmail.com>
2024-03-02 15:31:17 +0800

    introskip 1.2

--- plugins/adaptiveintroskip/__init__.py
+++ plugins/adaptiveintroskip/__init__.py
@@ -6,6 +6,7 @@ from app.schemas import WebhookEventInfo
 from app.schemas.types import EventType
 from .SkipHelper import *
 from app.log import logger
+from app.core.meta import MetaBase
 
 
 class AdaptiveIntroSkip(_PluginBase):
@@ -16,7 +17,7 @@ class AdaptiveIntroSkip(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/honue/MoviePilot-Plugins/main/icons/chapter.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
@@ -33,6 +34,7 @@ class AdaptiveIntroSkip(_PluginBase):
     _end_min: float = 6
     _include: str = ''
     _exclude: str = ''
+    _spec = ''
 
     def init_plugin(self, config: dict = None):
         if config:
@@ -42,6 +44,8 @@ class AdaptiveIntroSkip(_PluginBase):
             # 关键词
             self._include = config.get("include") or ''
             self._exclude = config.get("exclude") or ''
+            # 特别指定开始 结束时间
+            self._spec = config.get("spec") or ''
 
     @eventmanager.register(EventType.WebhookMessage)
     def hook(self, event: Event):
@@ -63,6 +67,17 @@ class AdaptiveIntroSkip(_PluginBase):
 
         logger.debug(event_info)
 
+        # 特别指定时间
+        spec_conf = self._spec.split('\n') if self._spec else []
+        for spec in spec_conf:
+            word, spec_begin, spec_end = spec.split('#')
+            if word in event_info.item_path:
+                self._begin_min = float(spec_begin)
+                self._end_min = float(spec_end)
+                logger.info(
+                    f"受关键词 {word} 限定，片头最晚结束于{self._begin_min}分钟，片尾最早开始于末尾{self._end_min}分钟")
+                break
+
         # 当前正在播放集的信息
         current_percentage = event_info.percentage
         current_video_item_id = get_current_video_item_id(item_id=event_info.item_id, playing_idx=event_info.episode_id)
@@ -77,20 +92,63 @@ class AdaptiveIntroSkip(_PluginBase):
         next_episode_ids = get_next_episode_ids(item_id=event_info.item_id, playing_idx=event_info.episode_id)
 
         if next_episode_ids:
+            # 存储最新片头位置，新集入库使用本数据
+            space_idx = event_info.item_name.index(' ')
+            series_name = event_info.item_name[:space_idx]
+            chapter_info = self.get_data(series_name) or {"item_id": event_info.item_id,
+                                                          "intro_end": 0,
+                                                          "credits_start": 0}
             # 当前播放时间（s）在[开始,begin_min]之间，且是暂停播放后，恢复播放的动作，标记片头
             if current_sec < (self._begin_min * 60) and event_info.event == 'playback.unpause':
                 intro_end = current_sec
                 # 批量标记之后的所有剧集，不影响已经看过的标记
                 for next_episode_id in next_episode_ids:
                     update_intro(next_episode_id, intro_end)
-                logger.info(f"{event_info.item_name} 后续剧集片头设置在 {int(intro_end / 60)}分{int(intro_end % 60)}秒 结束")
+                chapter_info['intro_end'] = intro_end
+                logger.info(
+                    f"{event_info.item_name} 后续剧集片头设置在 {int(intro_end / 60)}分{int(intro_end % 60)}秒 结束")
             # 当前播放时间（s）在[end_min,结束]之间，且是退出播放动作，标记片尾
             if current_sec > (total_sec - self._end_min * 60) and event_info.event == 'playback.stop':
                 credits_start = current_sec
                 for next_episode_id in next_episode_ids:
                     update_credits(next_episode_id, credits_start)
+                chapter_info['credits_start'] = credits_start
                 logger.info(
                     f"{event_info.item_name} 后续剧集片尾设置在 {int(credits_start / 60)}分{int(credits_start % 60)}秒 开始")
+
+            self.save_data(series_name, chapter_info)
+
+    @eventmanager.register(EventType.TransferComplete)
+    def episodes_hook(self, event: Event):
+        event_info: MetaBase = event.event_data.get("meta")
+        if event_info.total_episode > 5:
+            logger.debug(f"本事件只处理追更订阅")
+            return
+        series_name = event.event_data.get("mediainfo").title
+        if not series_name:
+            return
+        chapter_info: dict = self.get_data(series_name)
+        if not chapter_info:
+            logger.debug(f"{series_name} 没有设置过片头片尾信息，跳过")
+            return
+
+        # 新入库剧集的item_id
+        next_episode_ids = get_next_episode_ids(item_id=chapter_info.get("item_id"),
+                                                playing_idx=event_info.begin_episode)
+
+        if next_episode_ids:
+            # 批量标记新入库的剧集
+            intro_end = chapter_info.get("intro_end")
+            for next_episode_id in next_episode_ids:
+                update_intro(next_episode_id, intro_end)
+            logger.info(
+                f"{series_name} {event_info.season_episode} 新入库剧集，片头设置在 {int(intro_end / 60)}分{int(intro_end % 60)}秒 结束")
+
+            credits_start = chapter_info.get("credits_start")
+            for next_episode_id in next_episode_ids:
+                update_credits(next_episode_id, credits_start)
+            logger.info(
+                f"{series_name} {event_info.season_episode} 新入库剧集，片尾设置在 {int(credits_start / 60)}分{int(intro_end % 60)}秒 开始")
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         """
@@ -199,6 +257,29 @@ class AdaptiveIntroSkip(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
+                                    'md': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextarea',
+                                        'props': {
+                                            'model': 'spec',
+                                            'rows': 6,
+                                            'label': '特别指定开始结束时间段',
+                                            'placeholder': '用关键词特别指定开始结束时间段，格式：关键词#片头最大分钟#片尾最大分钟',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
                                 },
                                 'content': [
                                     {
@@ -223,16 +304,16 @@ class AdaptiveIntroSkip(_PluginBase):
                                             'variant': 'tonal',
                                             'text': '在片头限定时间内，暂停一下恢复播放能够将后续剧集的片头跳转全标记在这个点，在片尾限定时间内，片尾正常退出播放能够将后续剧集的片尾开始全标记在这个点，如有问题欢迎交流。'
                                         }
-                                    },{
+                                    }, {
                                         'component': 'VAlert',
                                         'props': {
                                             'type': 'success',
                                             'variant': 'tonal',
-                                            'text': '目前支持使用emby自带片头片尾信息的应该只有emby官方的客户端(包括小秘)，第三方播放器需要跟作者反馈请求支持这个功能。获取章节信息的API是存在的 /emby/Shows/${item_id}/Episodes'
+                                            'text': '目前支持使用emby自带片头片尾信息的应该有emby官方的客户端(包括小秘)、网页端，infuse(7.7.2)，vidhub(1.3.0)，第三方播放器需要跟作者反馈请求支持这个功能。获取章节信息的API是存在的 /emby/Shows/${item_id}/Episodes'
                                         }
                                     }
                                 ]
-                            },{
+                            }, {
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
@@ -257,7 +338,8 @@ class AdaptiveIntroSkip(_PluginBase):
             "begin_min": 4,
             "end_min": 6,
             "include": '',
-            "exclude": ''
+            "exclude": '',
+            "spec": ''
         }
 
     def get_state(self) -> bool:
