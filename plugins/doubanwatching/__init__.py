3028a417bf94386b1686bad98f74ed88f16f4ff8
honue <zouhy2001@gmail.com>
2024-05-27 13:10:34 +0800

    fix

--- plugins/doubanwatching/__init__.py
+++ plugins/doubanwatching/__init__.py
@@ -18,7 +18,7 @@ class DouBanWatching(_PluginBase):
     # 插件图标
     plugin_icon = "douban.png"
     # 插件版本
-    plugin_version = "1.7"
+    plugin_version = "1.8"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
