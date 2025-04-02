b6bcce13fc1fb982e026b5c1b2db0c3b1dc33d76
xinmeng96 <9sterev6@gmail.com>
2025-04-02 17:12:39 +0800

    空

--- plugins/anistrm/__init__.py
+++ plugins/anistrm/__init__.py
@@ -58,7 +58,7 @@ class ANiStrm(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/honue/MoviePilot-Plugins/main/icons/anistrm.png"
     # 插件版本
-    plugin_version = "2.4.2"
+    plugin_version = "2.4.3"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
